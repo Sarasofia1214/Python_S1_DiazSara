0f1edbced70c16911a1e58bbdc615abba7d09918
Sarasofia1214 <dasaro1206@gmail.com>
2025-02-05 06:32:59 -0500

    Update modulo1.py

--- Dia4/modulo1.py
+++ Dia4/modulo1.py
@@ -1,3 +1,4 @@
+##//Ejercicio1_DiazSara
 def ed (a,b,c,d,e):
     b[a-1]=c
     d[a-1]=e
@@ -14,3 +15,4 @@ def n (a,b):
     for i in range(len(a)):
             nombret=" ".join(a[i])+" "+" ".join(b[i])
             print (f"{i+1}, {nombret}") 
+## Sara Dìaz - T.I 1099741170
