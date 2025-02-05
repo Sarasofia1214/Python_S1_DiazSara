d5dc064c0c146f1257247ccab3178b7425bd8d01
Sarasofia1214 <dasaro1206@gmail.com>
2025-02-05 06:07:05 -0500

    Modulos dia4

---
+++ Dia4/modulo1.py
@@ -0,0 +1,16 @@
+def ed (a,b,c,d,e):
+    b[a-1]=c
+    d[a-1]=e
+
+def ana(a,b,c,d):
+     c.append(a)
+     d.append(b)
+
+def delete (a,b,c):
+     b.pop(a-1)  
+     c.pop(a-1)
+
+def n (a,b):
+    for i in range(len(a)):
+            nombret=" ".join(a[i])+" "+" ".join(b[i])
+            print (f"{i+1}, {nombret}") 
