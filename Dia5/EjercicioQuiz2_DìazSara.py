645b9ce5a2e33c8f9d5b9759344c0609d0836c8e
Sarasofia1214 <dasaro1206@gmail.com>
2025-02-07 06:38:36 -0500

    Caso estudio1

--- Dia5/EjercicioQuiz2_DìazSara.py
+++ Dia5/EjercicioQuiz2_DìazSara.py
@@ -1,22 +1,45 @@
 import json
 def abrirJSON():
     dicFinal={}
-    with open('./bbdd_Fut.json','r') as openFile:
+    with open('./1.json','r') as openFile:
         dicFinal=json.load(openFile)
     return dicFinal
 
 def guardarJSON(dic):
-    with open("./bbdd_Fut.json",'w') as outFile:
+    with open("./1.json",'w') as outFile:
         json.dump(dic,outFile)
     
-equiposFut={}
+inmuebles={}
 booleano=True
 while(booleano==True):
     print("Bienvenido a la inmobiliaria")
     print("Escribe el nùmero de la opciòn que deseas:")
-    print("1.Editar inmuebles")
-    print("2.Agregar inmuebles ")
-    print("3.Eliminar inmuebles")
-    print("4.Mostrar lista de inmuebles")
-    print("5.Salir del programa de la inmobiliaria")
+    print("1.Buscar inmuebles")
+    print("2.Editar inmuebles")
+    print("3.Agregar inmuebles ")
+    print("4.Eliminar inmuebles")
+    print("5.Mostrar lista de inmuebles")
+    print("6.Salir del programa de la inmobiliaria")
     opcionUsuario= int(input(":"))
+
+    if opcionUsuario==1:
+        inmuebles=abrirJSON()
+        print(inmuebles["inmuebles"])
+
+
+        presu=int(input("Escribe tu presupuesto:"))
+
+    elif opcionUsuario==2:
+        en=int(input("Cual inmueble quieres editar:"))
+        ed=input("Como sera el nuevo imueble:")
+
+        def funcionprecios():
+            inmuebles={}
+            inmuebles=abrirJSON()
+            print("lista antes)")
+        
+            for i in range(len(inmuebles["inmuebles"])):
+                print("Vivienda", i+1)
+                print("zona",inmuebles["inmuebles"][i]["zona"])
+                print("Año de construcciòn",inmuebles["inmuebles"][i]["zona"])
+                print("tamaño",inmuebles ["inmuebles"][i]["metros"] )
---
+++ casoestudio1.py
@@ -0,0 +1,14 @@
+import json 
+def abrirJSON():
+    dicFinal={}
+    with open(".casoestudi.json",'r') as openFile:
+        dicFinal=json.load(openFile)
+    return dicFinal
+
+def guardarJSON(dic):
+    with open("./casoestudi.json",'w') as outFile:
+        json.dump(dic,outFile)
+bo=True
+while bo==True
+print("Sistema digital de pedidos:")
+print("")
