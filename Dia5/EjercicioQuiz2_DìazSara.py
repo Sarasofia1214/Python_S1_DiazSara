0c73975b517980e8ed9dad3723cebe215694ac44
Sarasofia1214 <dasaro1206@gmail.com>
2025-02-14 10:12:54 -0500

    Update EjercicioQuiz2_DìazSara.py

--- Dia5/EjercicioQuiz2_DìazSara.py
+++ Dia5/EjercicioQuiz2_DìazSara.py
@@ -1,3 +1,4 @@
+from modulo import funcionprecios
 import json
 def abrirJSON():
     dicFinal={}
@@ -8,10 +9,13 @@ def abrirJSON():
 def guardarJSON(dic):
     with open("./1.json",'w') as outFile:
         json.dump(dic,outFile)
-    
+
+
+
+
 inmuebles={}
-booleano=True
-while(booleano==True):
+booleanito = True
+while(booleanito==True):
     print("Bienvenido a la inmobiliaria")
     print("Escribe el nùmero de la opciòn que deseas:")
     print("1.Buscar inmuebles")
@@ -22,24 +26,45 @@ while(booleano==True):
     print("6.Salir del programa de la inmobiliaria")
     opcionUsuario= int(input(":"))
 
-    if opcionUsuario==1:
-        inmuebles=abrirJSON()
-        print(inmuebles["inmuebles"])
+    if(opcionUsuario==2):
+        funcionprecios()   
 
+    elif(opcionUsuario==3):
+        inmuebleNuevo={}
+        zonaNuevo=input("Zona A/B:")
+        inmuebleNuevo["zona"]=zonaNuevo
+        anoNuevo=int(input("Año Construcción:"))
+        inmuebleNuevo["año"]=anoNuevo
+        metrosNuevo=int(input("Metros Construidos:"))
+        inmuebleNuevo["metros"]=metrosNuevo
+        habitacionesNuevo=int(input("Habitaciones:"))
+        inmuebleNuevo["habitaciones"]=habitacionesNuevo
+        garajeNuevo=int(input("¿Tiene garaje?(Si=1,No=0):"))
+        if(garajeNuevo==1):
+            garaje=1
+            inmuebleNuevo["garaje"]=True
+        else:
+            garaje=0
+            inmuebleNuevo["garaje"]=False
+        if(inmuebleNuevo["zona"]=="A"):
+    
+            precioFinalA=((inmuebleNuevo["metros"]) * 1000 + (inmuebleNuevo["habitaciones"]) * 5000 + garaje * 15000) * ((inmuebleNuevo["año"])/100)
+            inmuebleNuevo["precio"]=precioFinalA
+        else:
 
-        presu=int(input("Escribe tu presupuesto:"))
-
-    elif opcionUsuario==2:
-        en=int(input("Cual inmueble quieres editar:"))
-        ed=input("Como sera el nuevo imueble:")
-
-        def funcionprecios():
-            inmuebles={}
-            inmuebles=abrirJSON()
-            print("lista antes)")
-        
-            for i in range(len(inmuebles["inmuebles"])):
-                print("Vivienda", i+1)
-                print("zona",inmuebles["inmuebles"][i]["zona"])
-                print("Año de construcciòn",inmuebles["inmuebles"][i]["zona"])
-                print("tamaño",inmuebles ["inmuebles"][i]["metros"] )
+            precioFinalB=((inmuebleNuevo["metros"]) * 1000 + (inmuebleNuevo["habitaciones"]) * 5000 + garaje * 15000) * ((inmuebleNuevo["año"])/100)*1.5
+            inmuebleNuevo["precio"]=precioFinalB
+        print("Inmueble Nuevo")
+        print("Zona:",inmuebleNuevo["zona"])
+        print("Año de construcción:",inmuebleNuevo["año"])
+        print("Tamaño:",inmuebleNuevo["metros"],"m2")
+        print("Habitaciones:",inmuebleNuevo["habitaciones"])
+        if(inmuebleNuevo["garaje"]==True):
+            garaje=1
+            print("Garaje: Disponible")
+        else:
+            garaje=0
+            print("Garaje: No disponible")
+        print("Precio:",inmuebleNuevo["precio"])
+        inmuebles["inmuebles"].append(inmuebleNuevo)
+        guardarJSON(inmuebles)
